7fca5c94cd19a19e8b5a129dc5114aace032de5e
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2025-02-06 16:36:56 +0000

    Add basic player

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,17 +4,18 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
-	override public function create()
-	{
-		super.create();
+    override public function create()
+    {
+        super.create();
 
-                var text = new flixel.text.FlxText(0, 0, 0, "Test", 64);
-                text.screenCenter();
-                add(text);
-	}
+        add(new Player(112, 92));
+        var text = new flixel.text.FlxText(0, 0, 0, "Test", 64);
+        text.screenCenter();
+        add(text);
+    }
 
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
-	}
+    override public function update(elapsed:Float)
+    {
+        super.update(elapsed);
+    }
 }
---
+++ source/Player.hx
@@ -0,0 +1,11 @@
+package;
+
+import flixel.FlxSprite;
+
+class Player extends FlxSprite
+{
+    public function new(x: Int, y: Int)
+    {
+        super(x, y);
+    }
+}
